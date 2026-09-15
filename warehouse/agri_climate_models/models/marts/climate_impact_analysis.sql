d72c8cd341272bca1bef7029fecd2c49fd6f631c
NATO-dotcom <dnaliaka365@gmail.com>
2026-09-15 10:24:28 +0300

    feat:implemented seeds(added static csv file for crop_categories)

--- warehouse/agri_climate_models/models/marts/climate_impact_analysis.sql
+++ warehouse/agri_climate_models/models/marts/climate_impact_analysis.sql
@@ -6,11 +6,16 @@ WITH crops AS (
 
 weather AS (
     SELECT * FROM {{ ref('stg_weather') }}
+),
+
+categories AS (
+    SELECT * FROM {{ ref('crop_categories') }}
 )
 
 SELECT
     c.region_name,
     c.crop_type,
+    cat.crop_category,     -- Pulling the new category from our seed!
     c.harvest_year,
     c.yield_tons,
     w.total_rainfall_mm,
@@ -19,3 +24,5 @@ FROM crops c
 JOIN weather w
 ON c.region_id = w.region_id 
 AND c.harvest_year = w.harvest_year
+LEFT JOIN categories cat
+ON c.crop_type = cat.crop_type
